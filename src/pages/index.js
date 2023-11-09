0846024d247cceca168c9a20bb48f3230c471740
Abdul Aziz <dev.abdaziz@outlook.com>
2023-11-09 18:34:18 +0700

    baru bikin splash  screen

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from './Home'
+import Orders from './Orders'
+import Profile from './Profile'
+import Splash from './Splash'
+
+export {Home,Orders,Profile,Splash}
---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Home,Orders,Profile,Splash} from "../pages"
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} options={{headerShown: false}}/>
+        <Tab.Screen name="Orders" component={Orders} options={{headerShown: false}} />
+        <Tab.Screen name="Profile" component={Profile} options={{headerShown: false}} />
+    </Tab.Navigator>
+  )
+}
+
+const Router = () => {
+  return (
+    <Stack.Navigator initialRouteName='Splash'>
+        <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
+        <Stack.Screen name="MainApp" component={MainApp} options={{headerShown: false}} />
+      </Stack.Navigator>
+  )
+}
+
+export default Router
+
+const styles = StyleSheet.create({})
